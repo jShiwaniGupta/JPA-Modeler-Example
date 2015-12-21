d42236ff9a58faaa95066ca8fa381e6e5aa895a1
jShiwaniGupta <>
2015-12-22 00:24:16 +0530

    Blog Info : http://jpamodeler.blogspot.in/2015/11/namedstoredprocedurequery.html

---
+++ Named Stored Procedure/src/java/com/jpamodeler/nsp/Student.java
@@ -0,0 +1,100 @@
+package com.jpamodeler.nsp;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureParameter;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Shiwani Gupta <jShiwaniGupta@gmail.com>
+ */
+
+@Entity
+@Table(name = "student")
+@NamedStoredProcedureQuery(name = "FindStudentByBatch", procedureName = "studentAll", resultClasses = {Student.class}, parameters = {
+    @StoredProcedureParameter(name = "BATCH_NO", mode = ParameterMode.IN, type = Integer.class)})
+public class Student implements Serializable {
+
+    @Column(name = "SID", table = "student", nullable = false)
+    @Id
+    private Integer sid;
+
+    @Column(name = "LNAME", table = "student", length = 10)
+    @Basic
+    private String lname;
+
+    @Column(name = "EMAIL", table = "student", length = 30)
+    @Basic
+    private String email;
+
+    @Column(name = "BATCH", table = "student")
+    @Basic
+    private Integer batch;
+
+    @Column(name = "DEPT", table = "student", length = 10)
+    @Basic
+    private String dept;
+
+    @Column(name = "FNAME", table = "student", nullable = false, length = 10)
+    @Basic
+    private String fname;
+
+    public Student() {
+
+    }
+
+    public Integer getSid() {
+        return this.sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getLname() {
+        return this.lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getBatch() {
+        return this.batch;
+    }
+
+    public void setBatch(Integer batch) {
+        this.batch = batch;
+    }
+
+    public String getDept() {
+        return this.dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getFname() {
+        return this.fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+}
---
+++ Named Stored Procedure/src/java/com/jpamodeler/nsp/Tester.java
@@ -0,0 +1,39 @@
+package com.jpamodeler.nsp;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
+
+/**
+ *
+ * @author Shiwani Gupta <jShiwaniGupta@gmail.com>
+ */
+@Singleton
+@LocalBean
+@Startup
+public class Tester {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @PostConstruct
+    void init(){
+       
+        StoredProcedureQuery query = em.createNamedStoredProcedureQuery("FindStudentByBatch");
+        query.setParameter("BATCH_NO", 2012);
+        
+        List<Student> students = query.getResultList();
+        
+        System.out.println("Executing Stored Procedure Query ................");
+        for(Student student : students){
+            System.out.println("Student : " + student.getFname() +" "+ student.getLname());
+        }
+        
+        
+    }
+}
